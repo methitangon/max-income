e2ece644e549a337ee30ea63a1a7143aa8342e21
methitangon <methi2554@gmail.com>
2025-02-19 23:07:49 +0700

    add mock data for display

---
+++ lib/mock_income_source.dart
@@ -0,0 +1,7 @@
+import 'package:max_income/models/income_source.dart';
+
+final List<IncomeSource> mockIncomeSources = [
+  IncomeSource(id: 1, name: 'Rental Property A', type: 'rental'),
+  IncomeSource(id: 2, name: 'Delivery Truck 1', type: 'vehicle'),
+  IncomeSource(id: 3, name: 'Coffee Machine X', type: 'coffee'),
+];
